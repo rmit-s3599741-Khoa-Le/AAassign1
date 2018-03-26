ccb3a7d6cdb925a768ebfa017efe24083fd9df8d
rmit-s3599741-Khoa-Le <s3599741@student.rmit.edu.au>
2018-03-26 16:26:56 +1100

    Completed most methods
    Excepted for print and shortest distance

--- IndMatrix.java
+++ IndMatrix.java
@@ -11,11 +11,7 @@ import java.util.*;
  */
 public class IndMatrix <T extends Object> implements FriendshipGraph<T>
 {
-    /* private ArrayList<ArrayList<T>> matrixCol;
-    private ArrayList<T> matrixRow;
-    private int i = 0;
-    T notConn = (T)(Integer)0;
-    T conn = (T)(Integer)1; */
+
     ArrayList<ArrayList<Integer>> matrixCol;
     ArrayList<Integer> matrixRow;
     ArrayList<T> vert;
@@ -26,8 +22,6 @@ public class IndMatrix <T extends Object> implements FriendshipGraph<T>
     	// Implement me!
         matrixCol = new ArrayList<ArrayList<Integer>>();
         vert = new ArrayList<T>();
-        // matrixRow = new ArrayList<T>();
-        // matrixCol.add(matrixRow);
     } // end of IndMatrix()
     
     
@@ -38,39 +32,46 @@ public class IndMatrix <T extends Object> implements FriendshipGraph<T>
 	
     
     public void addEdge(T srcLabel, T tarLabel) {
-        // Implement me!
-        
+        // Implement me!    
         matrixRow = new ArrayList<Integer>(); // creating a new row
-        for(int i = 0; i < vert.size(); i++) {
-            
+        for(int i = 0; i < vert.size(); i++) {      
             if(vert.get(i).equals(srcLabel) || vert.get(i).equals(tarLabel)) { 
                 matrixRow.add(1);
             }
             else {
                 matrixRow.add(0);
             }
-        }
-        
+        }   
         matrixCol.add(matrixRow);
-        /**
-        matrixRow = new ArrayList<T>();     
-        for(int i = 0; i < matrixCol.get(0).size(); i++) {
-            if(matrixCol.get(0).contains(srcLabel) || matrixCol.get(0).contains(tarLabel)) {
-                matrixRow.add(conn);
-            }
-            else {
-                matrixRow.add(notConn);
-            }
-        }
-        matrixCol.add(matrixRow);
-        **/
     } // end of addEdge()
 	
 
     public ArrayList<T> neighbours(T vertLabel) {
-        ArrayList<T> neighbours = new ArrayList<T>();
-        
+        ArrayList<T> neighbours = new ArrayList<T>();      
         // Implement me!
+        int temp = 0; int count = 0;
+        
+        for(int i = 0; i < vert.size(); i++) { // find where vertLabel is located in vert
+            if(vert.get(i).equals(vertLabel)) {
+                temp = i;
+                break;
+            }
+        }
+        
+        for(int j = 0; j < matrixCol.size(); j++) {
+            if(temp < matrixCol.get(j).size()) {
+                if(matrixCol.get(j).get(temp) == 1) { // find if row with vertLabel
+                    for(int jj = 0; jj < matrixCol.get(j).size(); jj++) {
+                        if(matrixCol.get(j).get(jj) == 1 && jj != temp) { // find which vertex it's connected
+                            neighbours.add(vert.get(jj)); // add to array
+                        }
+                    }
+                }
+            }
+            else {
+                continue;
+            }
+        }
         
         return neighbours;
     } // end of neighbours()
@@ -79,19 +80,19 @@ public class IndMatrix <T extends Object> implements FriendshipGraph<T>
     public void removeVertex(T vertLabel) {
         // Implement me!
         int temp = 0;
-        for(int i = 0; i < vert.size(); i++) {
-            if(vert.get(i) == vertLabel) {
-                vert.remove(i);
+        for(int i = 0; i < vert.size(); i++) { // to find the position of deleted value
+            if(vert.get(i).equals(vertLabel)) {    
                 temp = i;
+                vert.remove(vertLabel);
             }
         }
         
-        for(int j = 0; j < matrixCol.size(); j++) {
-            if(matrixCol.get(j).get(temp) == 1) {
-                matrixCol.remove(j);
+        for(int j = 0; j < matrixCol.size(); j++) { // loop through to each row
+            if(matrixCol.get(j).get(temp) == 1) { // find if current row has deleted value
+                matrixCol.remove(j); // remove row
             }
             else {
-                matrixCol.get(j).remove(temp);
+                matrixCol.get(j).remove(temp); // delete column from row
             }
         }
         
@@ -100,6 +101,28 @@ public class IndMatrix <T extends Object> implements FriendshipGraph<T>
     
     public void removeEdge(T srcLabel, T tarLabel) {
         // Implement me!
+        int srcCount = 0;
+        int tarCount = 0;
+        int count = 0;
+        for(T object : vert) {
+            if(object.equals(srcLabel)) {
+                srcCount = count;
+            }
+            else if(object.equals(tarLabel)) {
+                tarCount = count;
+            }
+            if(srcCount != 0 && tarCount != 0) { break; } // don't need to go through every value
+            count++;
+        }
+        
+        for(int i = 0; i < matrixCol.size(); i++) { // loop through rows to delete certain column
+            if(matrixCol.get(i).get(srcCount) == 1 && matrixCol.get(i).get(tarCount) == 1) {
+                matrixCol.remove(i);
+                break;
+            }
+        }
+        
+        
         
     } // end of removeEdges()
 	
@@ -111,18 +134,23 @@ public class IndMatrix <T extends Object> implements FriendshipGraph<T>
             System.out.printf("%s ", vertex);
             os.print(vertex + " ");
         }
+        System.out.println();
     } // end of printVertices()
 	
     
     public void printEdges(PrintWriter os) {
-        // Implement me!
-        for(ArrayList<Integer> array : matrixCol) {
-            for(int i = 0; i < array.size(); i++) {
-                if(array.get(i) == 1) {
-                    String value = (String)vert.get(i);
+
+        int count = 0;
+        for(int i = 0; i < matrixCol.size(); i++) {
+            for(int j = 0; j < matrixCol.get(i).size(); j++) {
+                if(matrixCol.get(i).get(j) == 1) {
+                    count++;
+                    String value = (String)vert.get(j);
                     System.out.printf("%s ", value);
+                    if(count == 2) { break; }
                 }
             }
+            count = 0;
             System.out.println();
         }
     } // end of printEdges()
